96b3d2ba5c3a00f618ba6a97c7dc4528296ac8e3
Bart van Leeuwen <semanticfire@users.noreply.github.com>
2014-07-29 16:48:16 +0200

    Update snippets.js

--- snippets.js
+++ snippets.js
@@ -78,8 +78,13 @@ enyo.kind({
 		kind : "netage.ldproperty",
 		property : "http://xmlns.com/foaf/0.1/name",
 		components : [ {
-			name : "T",
-			kind : "input"
+			name : "B",
+			kind : "Drawer",
+			components : [{
+				name : "T",
+				kind : "input"	
+			}]
+			
 		} ]
 	} ],
 	rendered : function() {
@@ -121,6 +126,55 @@ enyo.kind({
 				name : "T",
 				kind : "input"
 			} ]
+		} ]
+	} ],
+	rendered : function() {
+		var kb = new $rdf.IndexedFormula();
+		kb.sf = new $rdf.Fetcher(kb);
+
+		// load data and fire up
+		kb.sf.nowOrWhenFetched(workUri, // the URI of the linked data resource
+		// we want to work with
+		undefined, function(ok, body) {
+			// get the subject for type foaf:Person
+			messages = kb.statementsMatching(undefined, new $rdf.Symbol(
+					'http://www.w3.org/1999/02/22-rdf-syntax-ns#type'),
+					new $rdf.Symbol('http://xmlns.com/foaf/0.1/Person'),
+					undefined, true);
+
+			// do the watterfall down of the first matching subject
+			this.waterfall("onGraphReady", {
+				graph : kb,
+				resource : messages[0].subject.uri
+			});
+		}.bind(this));
+
+	}
+});
+
+enyo.kind({
+	name : "C4",
+	kind : "FittableRows",
+	components : [ {
+		name : "P_1",
+		kind : "netage.ldproperty",
+		property : "http://xmlns.com/foaf/0.1/knows",
+		components : [ {
+			name : "P_2",
+			kind : "netage.ldproperty",
+			property : "http://xmlns.com/foaf/0.1/name",
+			components : [ {
+				name : "T_1",
+				kind : "input"
+			},
+			{
+			name : "P_3",
+			kind : "netage.ldproperty",
+			property : "http://xmlns.com/foaf/0.1/mbox",
+			components : [ {
+				name : "T_2",
+				kind : "input"
+			}]
 		} ]
 	} ],
 	rendered : function() {
